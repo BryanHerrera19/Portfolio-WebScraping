a695b1f93541eff5c24be5484c83a1c8fedac294
Bryan <b_herrera6@u.pacific.edu>
2023-02-09 14:40:05 -0800

    Successfully connected to database

--- MongoDatabase.py
+++ MongoDatabase.py
@@ -1,3 +1,4 @@
+import pymongo
 from pymongo import MongoClient
 
-client = pymongo.MongoClient("mongodb+srv://bherrera:y3p1EcmoqsrtzLCj@webscrapingdatabase.6yrgtql.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient('mongodb+srv://bherrera:y3p1EcmoqsrtzLCj@webscrapingdatabase.6yrgtql.mongodb.net/?retryWrites=true&w=majority')
