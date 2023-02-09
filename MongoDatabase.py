341eaca3fbfef1966dd45e586410c7ad70f043cc
Bryan <b_herrera6@u.pacific.edu>
2023-02-09 14:05:11 -0800

    added database connection url

--- MongoDatabase.py
+++ MongoDatabase.py
@@ -1,1 +1,3 @@
-import pymongo
+from pymongo import MongoClient
+
+client = pymongo.MongoClient("mongodb+srv://bherrera:y3p1EcmoqsrtzLCj@webscrapingdatabase.6yrgtql.mongodb.net/?retryWrites=true&w=majority")
