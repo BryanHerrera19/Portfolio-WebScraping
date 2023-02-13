934fe4c2972c29e4101eac91ecb6742f6d304e62
Bryan <b_herrera6@u.pacific.edu>
2023-02-13 07:19:00 -0800

    Created function to insert carHist and ownerHist

--- MongoDatabase.py
+++ MongoDatabase.py
@@ -4,6 +4,12 @@ from pymongo import MongoClient
 client = pymongo.MongoClient('mongodb+srv://bherrera:y3p1EcmoqsrtzLCj@webscrapingdatabase.6yrgtql.mongodb.net/?retryWrites=true&w=majority')
 wsdb = client["WebScrapingDB"]
 carCol = wsdb["Car_Info"]
+histCol = wsdb["Car_History"]
+ownerCol = wsdb["Owner_History"]
 
 def insertCarInfo(carInfoDict):
     carInfo = carCol.insert_one(carInfoDict)
+def insertHistInfo(carHistDict):
+    carHist = histCol.insert_one(carHistDict)
+def insertOwnerInfo(ownerHistDict):
+    ownerHist = ownerCol.insert_one(ownerHistDict)
