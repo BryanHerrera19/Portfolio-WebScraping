c4e328724f5693b3cb98edf00ce5e1f62dbee679
Bryan <b_herrera6@u.pacific.edu>
2023-02-13 06:28:06 -0800

    Removed test code

--- MongoDatabase.py
+++ MongoDatabase.py
@@ -5,9 +5,5 @@ client = pymongo.MongoClient('mongodb+srv://bherrera:y3p1EcmoqsrtzLCj@webscrapin
 wsdb = client["WebScrapingDB"]
 carCol = wsdb["Car_Info"]
 
-testDict = dict(manufacturer = "Ford", modelName = "Raptor", vin = 1564823, color = "Red", year = 2021, currentMileage = 10000)
-
 def insertCarInfo(carInfoDict):
     carInfo = carCol.insert_one(carInfoDict)
-
-insertCarInfo(testDict)
