0f3269bdc24429f691b242fb3b6947f368eef546
Bryan <b_herrera6@u.pacific.edu>
2023-02-13 00:44:24 -0800

    Test input successful ready for functions

--- MongoDatabase.py
+++ MongoDatabase.py
@@ -4,3 +4,6 @@ from pymongo import MongoClient
 client = pymongo.MongoClient('mongodb+srv://bherrera:y3p1EcmoqsrtzLCj@webscrapingdatabase.6yrgtql.mongodb.net/?retryWrites=true&w=majority')
 wsdb = client["WebScrapingDB"]
 carCol = wsdb["Car_Info"]
+
+testDict = dict(manufacturer = "Ford", modelName = "Raptor", vin = 1564823, color = "Red", year = 2021, currentMileage = 10000)
+test = carCol.insert_one(testDict)
