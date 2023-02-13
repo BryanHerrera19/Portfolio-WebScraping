98e20ef728a47deb756ecedc6f69801b87f676dd
Bryan <b_herrera6@u.pacific.edu>
2023-02-13 00:55:28 -0800

    Created function for carInfo data and tested

--- MongoDatabase.py
+++ MongoDatabase.py
@@ -6,4 +6,8 @@ wsdb = client["WebScrapingDB"]
 carCol = wsdb["Car_Info"]
 
 testDict = dict(manufacturer = "Ford", modelName = "Raptor", vin = 1564823, color = "Red", year = 2021, currentMileage = 10000)
-test = carCol.insert_one(testDict)
+
+def insertCarInfo(carInfoDict):
+    carInfo = carCol.insert_one(carInfoDict)
+
+insertCarInfo(testDict)
