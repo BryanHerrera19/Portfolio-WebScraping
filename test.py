cf303fdb1b387f82d3e3fdbe02a5fefc2a10c0f1
sloan530 <g_kim22@u.pacific.edu>
2023-03-08 12:37:05 -0800

    testing out different method

--- test.py
+++ test.py
@@ -1,41 +1,53 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import chromedriver_autoinstaller
+import subprocess
 import vehicle
 import re
-import time
-import automator
-from selenium.webdriver.common.proxy import Proxy, ProxyType
-import undetected_chromedriver as uc
-from selenium_stealth import stealth
+import shutil
 
+# using debugger and cookie (by not deleting them), trick the system to get undetected as a bot
+# however, after few times, gets blocked
+# attempted to delete the cookie and use extension to solve captcha but debugger mode does not support chrom extension
 
-options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_argument("--window-size=1920x1080")
+try:
+    shutil.rmtree(r"C:\chrometemp")  # remove Cookie, Cache files
+except FileNotFoundError:
+    pass
 
-url = "https://www.carvana.com/vehicle/2644429"
+try:
+    subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 '
+                     r'--user-data-dir="C:\chrometemp"')   # Open the debugger chrome
+    
+except FileNotFoundError:
+    subprocess.Popen(r'C:\Users\binsu\AppData\Local\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 '
+                     r'--user-data-dir="C:\chrometemp"')
 
-browser = webdriver.Chrome(options=options)
+option = Options()
 
-stealth(browser,
-        languages=["en-US", "en"],
-        vendor="Google Inc.",
-        platform="Win32",
-        webgl_vendor="Intel Inc.",
-        renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True,
-        )
+option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+option.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36')
+option.add_extension(r'C:\Users\gkim5\OneDrive\문서\2023 Spring\COMP 195\senior-project-spring-2023-web-scraping\extension.crx')
 
+chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+try:
+    browser = webdriver.Chrome(options=option)
+    
+except:
+    chromedriver_autoinstaller.install(True)
+    browser = webdriver.Chrome(options=option)
+browser.implicitly_wait(10)
+
+url = 'https://www.carvana.com/vehicle/2452937'
 image = ''
-fuel = 'Diesel'
+fuel = 'Gas'
 
+browser.maximize_window()
+browser.get(url)
 newVehicle = vehicle.Vehicle(url, image, fuel)
 
-browser.get(url)
 browser.execute_script("window.scrollTo(0, 500)") 
 time.sleep(5)
 elems_p = browser.find_elements(By.TAG_NAME, 'p')
@@ -76,69 +88,26 @@ elem_vin = browser.find_element(By.XPATH,'//a[contains(@href,"/VehicleHistory/")
 vin_history_url = elem_vin.get_attribute("href")
 newVehicle.transmission_color_vinHistoryURL_setter(transmission_type, color, vin_history_url)
 
-print("vehicle url: " + newVehicle.url)
-print("year: " + str(newVehicle.year))
-print("company: "+ newVehicle.company)
-print("model: "+ newVehicle.model)
-print("vin history url: " + newVehicle.vin_history_url)
-print("image url: " + newVehicle.image)
-print("miles : " + str(newVehicle.miles))
-print("price : " + str(newVehicle.price))
-print("transmission type : " + newVehicle.transmission_type)
-print("Exterior color: " + newVehicle.color)
-print("fuel type: " + newVehicle.fuel)
+time.sleep(10)
+browser.find_element(By.XPATH,'//a[contains(@data-analytics-id, "Specifications - Previous Use Disclaimer")]').click()
+# click vin history link
+time.sleep(10)
+browser.switch_to.window(browser.window_handles[1])
+elems_div = browser.find_elements(By.CLASS_NAME,'history-overview-cell')
+elems_div_texts = []
+for elem in elems_div:
+       elems_div_texts.append(elem.text)
 
+num_owners = list(filter(lambda v: re.match(r'.* Previous owners', v), elems_div_texts))[0]
+accidents = list(filter(lambda v: re.match(r'.*accidents.*', v), elems_div_texts))[0]
+last_state = list(filter(lambda v: re.match(r'.*owned in.*', v), elems_div_texts))[0]
+regular_oil_changes = list(filter(lambda v: re.match(r'Regular oil changes', v), elems_div_texts))[0]
+usage = list(filter(lambda v: re.match(r'*.vehicle', v), elems_div_texts))[0]
 
-
-
-browser.quit()
-# elem_vin = browser.find_element(By.XPATH,'//a[contains(@data-analytics-id,"Specifications - Previous Use Disclaimer")]')
-# elem_vin_location = elem_vin.location
-# print(elem_vin_location)
-# elem_vin.click()
-
-
-# elems = browser.find_elements(By.TAG_NAME, 'p')
-# elems_p_texts = []
-# for elem in elems:
-#     elems_p_texts.append(elem.text)
-
-# print(elems_p_texts)
-# year_model_text = list(filter(lambda v: re.match('\d\d\d\d.*', v), elems_p_texts))[0]
-# print(year_model_text)
-# miles_text = list(filter(lambda v: re.match('\d+,\d+ Miles', v), elems_p_texts))[0]
-# print(miles_text)
-# if (re.match(r'\d+,\d+', '$15,590')):
-#     print('ey')
-# price_text = list(filter(lambda v: re.match('$\d+,\d+', v), elems_p_texts))
-# print(price_text)
-# txt = elems[2].text.split(' ')[0].replace(',', '')
-# price = int(elems[4].text.strip('$').replace(',', ''))
-# print(txt)
-# print(elems[8].text.split(',')[0])
-
-
-# # gets all the attributes
-# attrs = browser.execute_script('var items = {}; for (index = 0; index < arguments[0].attributes.length; ++index) { items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value }; return items;', a)
-# print(attrs)
-
-
-# browser.execute_script("window.scrollTo(0, 2194)") 
-# elems_li = browser.find_elements(By.CLASS_NAME, 'sc-4436f562-5')
-# elems_li_texts = []
-# for elem in elems_li:
-#     elems_li_texts.append(elem.text)
-# print(elems_li_texts)
-
-# if list(filter(lambda v: re.match(r'^Auto.*', v), elems_li_texts)) == []:
-#     if list(filter(lambda v: re.match(r'^Manual.*', v), elems_li_texts)) == []:
-#         if list(filter(lambda v: re.match(r'^CVT.*', v), elems_li_texts)) == []:
-#             transmission_type = None
-#         else:
-#             transmission_type = 'CVT'
-#     else:
-#         transmission_type = 'Manual'
-# else:
-#     transmission_type = 'Automatic'
-# print(transmission_type)
-
+print(num_owners)
+print(accidents)
+print(last_state)
+print(regular_oil_changes)
+print(usage)
+time.sleep(100)
+browser.close()
