3699d0ee8a8805b90fba2fae8a7f170a6bde02d3
Tran Hoang <79217827+TranHoang3105@users.noreply.github.com>
2023-03-22 12:19:16 -0700

    added CarInfo screen. homebutton

--- GUI.py
+++ GUI.py
@@ -12,6 +12,7 @@ class MainScreen(QMainWindow):
         super(MainScreen,self).__init__()
         loadUi("MainScreen.ui", self)
         self.Sbutton.clicked.connect(self.gotoSearchScreen)
+        self.Hbutton.clicked.connect(self.gotoCarInfo)
         self.quit_button.clicked.connect(self.quit_func)
         self.show()
 
@@ -20,6 +21,10 @@ class MainScreen(QMainWindow):
         widget.addWidget(sScreen)
         widget.setCurrentIndex(widget.currentIndex() + 1)
 
+    def gotoCarInfo(self):
+        cScreen = CarInfo()
+        widget.addWidget(cScreen)
+        widget.setCurrentIndex(widget.currentIndex() + 2)
     def quit_func(self):
         sys.exit(app.exec())
 
@@ -29,20 +34,43 @@ class FilterScreen(QMainWindow):
     def __init__(self):
         super(FilterScreen, self).__init__()
         loadUi("FilterScreen.ui", self)
+        self.Hbutton.clicked.connect(self.gotoHomeScreen)
         self.quit_button.clicked.connect(self.quit_func)
 
     def quit_func(self):
         sys.exit(app.exec())
 
+    def gotoHomeScreen(self):
+        hScreen = MainScreen()
+        widget.addWidget(hScreen)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+class CarInfo(QMainWindow):
+    def __init__(self):
+        super(CarInfo, self).__init__()
+        loadUi("CarInfo.ui",self)
+        self.Hbutton.clicked.connect(self.gotoHomeScreen)
+        self.quit_button.clicked.connect(self.quit_func)
+
+    def gotoHomeScreen(self):
+        hScreen = MainScreen()
+        widget.addWidget(hScreen)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+    def quit_func(self):
+        sys.exit(app.exec())
+
+
 # Create application
 app = QApplication(sys.argv)
 mc = MainScreen()
 sc = FilterScreen()
+ci = CarInfo()
 
 # Creat widgets to stores multiple windows/screens
 widget = QStackedWidget()
 widget.addWidget(mc)
 widget.addWidget(sc)
+widget.addWidget(ci)
 widget.setFixedSize(1200,700)
 
 # No windows bar/Status bar
