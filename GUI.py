2bfcd1a6a91f6ecda87daf26810efab3b2dd5178
Tran Hoang <79217827+TranHoang3105@users.noreply.github.com>
2023-03-31 01:13:17 -0700

    checkbox is working now

--- GUI.py
+++ GUI.py
@@ -5,11 +5,17 @@ from PyQt5 import QtCore
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+import requests
+from PIL import Image
+from io import BytesIO
+
 from mongoDB import *
+
+
 # Main Screen
 class MainScreen(QMainWindow):
     def __init__(self):
-        super(MainScreen,self).__init__()
+        super(MainScreen, self).__init__()
         loadUi("MainScreen.ui", self)
         self.Sbutton.clicked.connect(self.gotoSearchScreen)
         self.Hbutton.clicked.connect(self.gotoCarInfo)
@@ -25,6 +31,7 @@ class MainScreen(QMainWindow):
         cScreen = CarInfo()
         widget.addWidget(cScreen)
         widget.setCurrentIndex(widget.currentIndex() + 2)
+
     def quit_func(self):
         sys.exit(app.exec())
 
@@ -37,11 +44,25 @@ class FilterScreen(QMainWindow):
         self.Hbutton.clicked.connect(self.gotoHomeScreen)
         self.quit_button.clicked.connect(self.quit_func)
 
+        # Filter for later use(not for now)
+        '''self.price_check1.setStyleSheet("QCheckBox" "{"
+                                        "font-size: 25px;"
+                                        "spacing:15px;"
+                                        "color:white;"
+                                        "font-weight:bold;"
+                                        "font-family: Open Sans; "
+                                        "}""QCheckBox::indicator" "{"
+                                        "width:25px;"
+                                        "height:25px;"
+                                        "background-color:white;"
+                                        "border-radius: 8px;"
+                                        "}" "QCheckBox::indicator:unchecked" "{"
+	                                    "background-color:red;"
+                                        "}")
+        '''
+
         # Car display(random 5, no filter)
-        records = getRecords(5)
-        # Set row
-        row_count = len(records)
-        col_count = len(records[0])
+
         self.cdt.setRowCount(row_count)
         self.cdt.setColumnCount(col_count)
         self.cdt.setHorizontalHeaderLabels((records[0].keys()))
@@ -49,7 +70,7 @@ class FilterScreen(QMainWindow):
         idx = 0;
         # Adding/Showing data to the table
         for x in records:
-            self.cdt.setItem(idx,1, QtWidgets.QTableWidgetItem(x['manufacturer']))
+            self.cdt.setItem(idx, 1, QtWidgets.QTableWidgetItem(x['manufacturer']))
             self.cdt.setItem(idx, 2, QtWidgets.QTableWidgetItem(x['modelName']))
             self.cdt.setItem(idx, 3, QtWidgets.QTableWidgetItem(x['vin']))
             self.cdt.setItem(idx, 4, QtWidgets.QTableWidgetItem(x['color']))
@@ -60,33 +81,39 @@ class FilterScreen(QMainWindow):
             self.cdt.setItem(idx, 9, QtWidgets.QTableWidgetItem(x['fuelType']))
             self.cdt.setItem(idx, 10, QtWidgets.QTableWidgetItem(x['image']))
             self.cdt.setItem(idx, 11, QtWidgets.QTableWidgetItem(x['url']))
-            idx +=1
-
-
+            idx += 1
 
     def quit_func(self):
         sys.exit(app.exec())
-
-    def gotoHomeScreen(self):
-        hScreen = MainScreen()
-        widget.addWidget(hScreen)
-        widget.setCurrentIndex(widget.currentIndex()+1)
-
-class CarInfo(QMainWindow):
-    def __init__(self):
-        super(CarInfo, self).__init__()
-        loadUi("CarInfo.ui",self)
-        self.Hbutton.clicked.connect(self.gotoHomeScreen)
-        self.quit_button.clicked.connect(self.quit_func)
 
     def gotoHomeScreen(self):
         hScreen = MainScreen()
         widget.addWidget(hScreen)
         widget.setCurrentIndex(widget.currentIndex() + 1)
+
+
+class CarInfo(QMainWindow):
+    def __init__(self):
+        super(CarInfo, self).__init__()
+        loadUi("CarInfo.ui", self)
+        self.Hbutton.clicked.connect(self.gotoHomeScreen)
+        self.quit_button.clicked.connect(self.quit_func)
+
+        # Data
+
+    def gotoHomeScreen(self):
+        hScreen = MainScreen()
+        widget.addWidget(hScreen)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
+
     def quit_func(self):
         sys.exit(app.exec())
 
-#Data base
+
+# Data base
+records = getRecords(10)
+row_count = len(records)
+col_count = len(records[0])
 
 # Create application
 app = QApplication(sys.argv)
@@ -99,7 +126,7 @@ widget = QStackedWidget()
 widget.addWidget(mc)
 widget.addWidget(sc)
 widget.addWidget(ci)
-widget.setFixedSize(1200,700)
+widget.setFixedSize(1200, 700)
 
 # No windows bar/Status bar
 widget.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
--- mongoDB.py
+++ mongoDB.py
@@ -2,6 +2,8 @@ import vehicle
 import pymongo
 from pymongo import MongoClient
 
+
+
 client = pymongo.MongoClient('mongodb+srv://bherrera:y3p1EcmoqsrtzLCj@webscrapingdatabase.6yrgtql.mongodb.net/?retryWrites=true&w=majority')
 wsdb = client["WebScrapingDB"]
 carCol = wsdb["Car_Info"]
